d06390a41aad15dc880860aff40905da6001eb36
Caesar Developer <caesararg.dev@gmail.com>
2024-11-29 12:14:47 -0600

    Change made by local user to amplify

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,24 @@
 
 import "./../app/app.css";
 import "@aws-amplify/ui-react/styles.css";
+import Image from "next/image";
+
+import imglogo from "@/public/images/logo-white.png";
 
 export default function App() {
   const stage = process.env.NEXT_PUBLIC_STAGE || "Dev";
 
   return (
     <main>
-      <h1>SHIFRA-SPA AMPLIFY ENVIRONMENT</h1>
       <center>
+        <Image
+          src={imglogo.src}
+          alt=""
+          width={350} // Required for layout="intrinsic" or "responsive"
+          height={200}
+        />
+        <h1>SHIFRA-SPA AMPLIFY ENVIRONMENT</h1>
+
         <h2>🚀 App successfully hosted 🚀.</h2>
         <h2>Current Environment: {stage}</h2>
       </center>
