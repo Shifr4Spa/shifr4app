a20a84621481579595b5432028a64cf3c143de3b
Caesar Developer <caesararg.dev@gmail.com>
2024-12-05 11:33:23 -0600

    Fix deploy issue 1

--- app/(client)/page.tsx
+++ app/(client)/page.tsx
@@ -1,4 +1,3 @@
-import { client } from "@/sanity/lib/client";
 import Header from "../components/Header";
 import { Post } from "../utils/interface";
 import PostComponent from "../components/PostComponent";
@@ -8,30 +7,9 @@ import ServiceSection from "./(sections)/ServiceSection";
 import FooterSection from "./(sections)/FooterSection";
 import ContactSection from "./(sections)/ContactSection";
 
-async function getPosts() {
-  const query = `
-  *[_type == "post"] {
-    title,
-    slug,
-    publishedAt,
-    excerpt,
-    tags[]-> {
-      _id,
-      slug,
-      name
-    }
-  }
-  `;
-  const data = await client.fetch(query);
-  return data;
-}
-
 export const revalidate = 60;
 
 export default async function Home() {
-  const posts: Post[] = await getPosts();
-  console.log(posts, "posts");
-
   return (
     <div className="min-h-screen">
       <HeroSection />
