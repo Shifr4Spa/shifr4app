47663f325b9cc31f98fab09aaa9e39e47daa645e
Caesar Developer <caesararg.dev@gmail.com>
2024-12-12 14:09:13 -0600

    Add new items into services and products

--- app/(client)/(sections)/ServiceSection.tsx
+++ app/(client)/(sections)/ServiceSection.tsx
@@ -99,54 +99,6 @@ function ServiceSection() {
         </div>
       </div>
 
-      <section className="py-14">
-        <div className="max-w-screen-xl mx-auto px-4 md:px-8">
-          <div className="max-w-xl sm:text-center md:mx-auto">
-            <h3
-              className={`font-frank text-gray-800 text-3xl font-normal sm:text-4xl`}
-            >
-              Lo Que Nuestros Clientes Dicen
-            </h3>
-            <p className="fontRoboto mt-3 font-thin">
-              Descubre las experiencias de aquellos que confiaron en nuestros
-              servicios de spa. Sus testimonios reflejan nuestro compromiso con
-              el bienestar, la relajación y la satisfacción total de cada
-              cliente.
-            </p>
-          </div>
-          <div className="mt-12">
-            <ul className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
-              {testimonials.map((item, idx) => (
-                <li key={idx} className=" p-4 rounded-xl">
-                  <figure>
-                    <div className="flex items-center gap-x-4">
-                      <Image
-                        width={100}
-                        height={100}
-                        alt="avatar"
-                        src={item.avatar}
-                        className="w-16 h-16 rounded-full"
-                      />
-                      <div>
-                        <span className="block text-gray-800 font-semibold">
-                          {item.name}
-                        </span>
-                        <span className="block text-gray-600 text-sm mt-0.5">
-                          {item.title}
-                        </span>
-                      </div>
-                    </div>
-                    <blockquote>
-                      <p className="mt-6 text-gray-700">{item.quote}</p>
-                    </blockquote>
-                  </figure>
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
-      </section>
-
       <div className="mt-6">
         <div className="max-w-4xl mx-auto relative">
           <div className="text-center">
